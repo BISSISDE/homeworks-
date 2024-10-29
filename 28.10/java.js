c0244dc441bd7b09b6465b1427117e29b17eac1c
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2024-10-29 17:49:19 +0600

    add hw 28.10

---
+++ 28.10/java.js
@@ -0,0 +1,8 @@
+console.log(
+  "1)JavaScript is a programming language for interactive web content."
+);
+console.log("2)CTRL+/");
+console.log(
+  "3) document.getElementById('date').innerHTML = today.toDateString();"
+);
+console.log("4)Internal and External ішкі және сыртқы");
