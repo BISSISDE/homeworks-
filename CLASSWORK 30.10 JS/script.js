2e19630abf5d4d797da4f3806c2f1f9bae27aa98
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2024-10-30 17:46:25 +0600

    class work

---
+++ CLASSWORK 30.10 JS/script.js
@@ -0,0 +1,28 @@
+let name = "Aiganym";
+let surname = "Nurkadyr";
+const birthday = "24.05.2010";
+const school = 172;
+let weight = "48kg"
+
+console.log("Аты", name)
+console.log("Last name is", surname)
+console.log("Birthday" , birthday);
+console.log("School", school);
+console.log("Weight",weight)
+
+
+let notebookName = "Huawei";
+let processor = "13th Gen Intel(R) Core(TM) i5-13420H   2.10 GHz"; 
+let size = "16,0 ГБ (доступно: 15,7 ГБ)";
+let color = "gray"
+let todayWeather = "Cloudly"
+let currentTime = "16:35"
+
+console.log("notebookname", notebookName);
+console.log("processor", processor);
+console.log("size", size);
+console.log("color", color);
+console.log("todayWeater", todayWeather);
+console.log("currentTime", currentTime);
+
+
