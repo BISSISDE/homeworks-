dc7b4be5732dfc3422d11d10051480a36686b636
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2024-10-30 21:08:42 +0600

    30.10 hw

---
+++ HOMEWORK 30.10 JS/script.js
@@ -0,0 +1,11 @@
+const modelName = "Daewoo Matiz";
+let color = "green";
+const wheelsCount = 4;
+let isOffRoad = "no";
+let rating = 3.7;
+
+console.log("Model Name is", modelName,)
+console.log("Color is", color);
+console.log("WheelsCount are", wheelsCount);
+console.log("IsOffRoad?", isOffRoad);
+console.log("Rating is", rating);
