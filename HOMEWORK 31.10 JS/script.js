509cba8b727dcc3ae8b8913a82bde5b414d2bdc0
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2024-10-31 21:28:42 +0600

    HW 31.10

---
+++ HOMEWORK 31.10 JS/script.js
@@ -0,0 +1,16 @@
+// 1 task
+let slicesPerPizza = 8;
+let numberOfPizza = 5;
+var totalSlices = slicesPerPizza * numberOfPizza;
+console.log("1 task result is",totalSlices);
+
+// 2 task
+let totalChocolates = 24;
+let numberOfFriends = 6;
+var chocolatesPerFriend = totalChocolates / numberOfFriends;
+console.log("2 task result is",chocolatesPerFriend);
+
+// 3task
+plantHeight = 10;
+plantHeight += 2;
+console.log("3 task result is", plantHeight);
