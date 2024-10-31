9db90062c66266958cdc9d7e71442fde0408c9d1
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2024-10-31 17:33:11 +0600

    CLASSWORK 31.10

---
+++ CLASSWORK 31.10 JS/script.js
@@ -0,0 +1,13 @@
+// let a = 3;
+// let b = 10;
+// let c = 8;
+// let z = 6;
+// let res = (a * b) / c / (z - 3);
+// res %= 2;
+// console.log(res);
+
+let a = 3;
+let b = 10;
+let c = 8;
+let result = 56 * (((a / b) * 45) / (c - b - a) / 10);
+console.log(result);
