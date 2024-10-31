f99e35bf9b1f6c9bbac9ba4988d20024277c6098
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2024-10-31 20:08:47 +0600

    2task

--- CLASSWORK 31.10 JS/script.js
+++ CLASSWORK 31.10 JS/script.js
@@ -6,8 +6,18 @@
 // res %= 2;
 // console.log(res);
 
-let a = 3;
+// let a = 3;
+// let b = 10;
+// let c = 8;
+// let result = 56 * (((a / b) * 45) / (c - b - a) / 10);
+// console.log(result);
+
+let a = 20;
 let b = 10;
-let c = 8;
-let result = 56 * (((a / b) * 45) / (c - b - a) / 10);
-console.log(result);
+P = 2 * (a + b);
+S = a * b;
+console.log("P=", P);
+console.log("S=", S);
+
+
+
