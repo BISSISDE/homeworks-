2f409a4ff963727df712efd5553d4ee4d707bac6
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2024-12-10 23:43:58 +0600

    10.12 HW

---
+++ HOMEWORK 10.12 JS HTML CSS/script.js
@@ -0,0 +1,17 @@
+const heading = document.querySelector(".title")
+heading.textContent = "DOM (Document Object Model) в JavaScript";
+
+const aboutDOM = document.querySelector(".information")
+aboutDOM.textContent =
+  "DOM (Document Object Model) — веб-документтерді басқаруға арналған интерфейс. ";
+
+const lern = document.querySelector(".goodway")
+lern.textContent =
+  "Динамикалық мазмұн: Бетті қайта жүктемей-ақ мазмұнды өзгерту.";
+
+const interestinginfo = document.querySelector(".fact")
+interestinginfo.innerHTML =
+  "<em>Веб-беттердің құрылымын ағаш тәрізді иерархияда көрсетеді</em>";
+
+const name = document.querySelector(".onclick")
+name.textContent = "My name is Aiganym"
