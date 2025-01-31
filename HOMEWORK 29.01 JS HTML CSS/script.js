55d47367d870ef54b942f95142fa2ea5cc30c1f0
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2025-01-31 15:18:49 +0500

    i add SavedColor()

--- HOMEWORK 29.01 JS HTML CSS/script.js
+++ HOMEWORK 29.01 JS HTML CSS/script.js
@@ -4,13 +4,25 @@ function SetItem() {
   const color = colorInput.value;
   sessionStorage.setItem("color", color);
 }
-SetItem();
+
 function GetItem() {
   const GetColor = sessionStorage.getItem("color");
-  document.body.style.backgroundColor = colorInput.value;
+  document.body.style.backgroundColor = GetColor;
 }
+
 function Clear() {
   sessionStorage.removeItem("color");
-  document.body.style.backgroundColor = "white";
-  colorInput.value = "";
+  document.body.style.backgroundColor = "white"; 
+  colorInput.value = ""; 
 }
+
+function SavedColor() {
+  const savedColor = sessionStorage.getItem("color");
+  if (savedColor) {
+    document.body.style.backgroundColor = savedColor; 
+    colorInput.value = savedColor;
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+}
+window.onload = SavedColor;
